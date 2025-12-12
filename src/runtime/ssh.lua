1449fa5acbca535d770e0c8fd248e807eb969895
saher <msaher.shair@gmail.com>
2025-12-12 14:17:05 +0300

    add ssh.Auth helpers

--- src/runtime/ssh.lua
+++ src/runtime/ssh.lua
@@ -6,6 +6,39 @@ local SshCmd = lash._get_metatable("SshCmd").__index
 --- @field exit_signal? string
 --- @field stderr? string
 
+--- @param opts { password: string }
+function ssh.AuthPassword(opts)
+    return {
+        type = "password",
+        password = opts.password,
+    }
+end
+
+--- @param opts { passphrase: string? }
+function ssh.AuthPublickey(opts)
+    return {
+        type = "publickey",
+        passphrase = opts.passphrase
+    }
+end
+
+--- @param opts { publickey: string, privatekey: string, passphrase: string?}
+function ssh.AuthPublickeyFile(opts)
+    return {
+        type = "publickey_file",
+        publickey = opts.publickey,
+        privatekey = opts.privatekey,
+        passphrase = opts.passphrase,
+    }
+end
+
+--- @param opts {}
+local function AuthAgent()
+    return {
+        type = "agent",
+    }
+end
+
 --- @return (string, lash.ssh.ExitState)
 function SshCmd:output()
     assert(self.stdout == nil, "Cannot redirect stdout when it's being captured")
